cd945e20765b6b80d40037800b62996b2491baa9
Mike Klein <mtklein@gmail.com>
2025-06-02 00:14:14 +0800

    more hacking

--- pong.c
+++ pong.c
@@ -14,19 +14,45 @@ static const byte palette[16] = {
     0,0,0,0,
 };
 
-static const byte text[]="Hello Pong!";
+static byte vram_update[] = {
+    MSB(NTADR_A(10,14)) | NT_UPD_HORZ,
+    LSB(NTADR_A(10,14)),
+    11,
+    'H', 'e', 'l', 'l', 'o',  ' ',  'P', 'i', 'n', 'g',  '!',
+    NT_UPD_EOF,
+};
+
+static byte state     = 0;
+static byte frame_mod = 0;
 
 void main(void) {
     ppu_off();
     {
         pal_bg(palette);
-
-        vram_adr  (NTADR_A(10,14));
-        vram_write(text, sizeof text);
     }
     ppu_on_all();
 
     while (1) {
+        if (++frame_mod == 60) {
+            frame_mod = 0;
+        }
 
+        switch (state) {
+            case 0:
+                set_vram_update(vram_update);
+                if (frame_mod == 0) {
+                    vram_update[10] = 'o';
+                    state = 1;
+                }
+                break;
+
+            case 1:
+                set_vram_update(NULL);
+                if (frame_mod ==  0) { pal_bg_bright(3); }
+                if (frame_mod == 30) { pal_bg_bright(4); }
+                break;
+        }
+
+        ppu_wait_nmi();
     }
 }
