b358830d16461b722c26b89dcd8f0301c2f7693e
Mike Klein <mtklein@gmail.com>
2025-06-01 16:17:59 +0800

    hello!

--- pong.c
+++ pong.c
@@ -7,5 +7,26 @@ byte oam_off;
 #pragma data-name(pop)
 #pragma  bss-name(pop)
 
+static const byte palette[16] = {
+    0x0f,0,0,0x31,
+    0,0,0,0,
+    0,0,0,0,
+    0,0,0,0,
+};
+
+static const byte text[]="Hello Pong!";
+
 void main(void) {
+    ppu_off();
+    {
+        pal_bg(palette);
+
+        vram_adr  (NTADR_A(10,14));
+        vram_write(text, sizeof text);
+    }
+    ppu_on_all();
+
+    while (1) {
+
+    }
 }
